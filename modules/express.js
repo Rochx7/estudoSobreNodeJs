3cfe6e96f7307b230af5ae165a22d897ddc80446
rochx7 <rochagamer55@hotmail.com>
2022-03-16 20:44:50 -0300

    feat: ejs

--- modules/express.js
+++ modules/express.js
@@ -5,6 +5,15 @@ const app = express()
 
 app.use(express.json())
 
+app.set('view engine', 'ejs')
+app.set('views', 'src/views')
+
+app.get('/views/users', async (req, res) => {
+  const users = await UserModel.find({})
+
+  res.render('index', { users })
+})
+
 app.use((req, res, next) => {
   console.log(`Request Type: ${req.method}`)
   console.log(`Content Type: ${req.headers['content-type']}`)
